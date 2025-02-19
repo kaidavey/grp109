378ae9ab51f8edb936dd358c74376deaaee3ff1f
ValeriaV-8 <james010698@gmail.com>
2025-02-18 19:36:22 -0800

    Create iterationLink.js

---
+++ team_project/scripts/iterationLink.js
@@ -0,0 +1,14 @@
+function newlink(){
+    //Create a element
+    let a = document.createElement('a');
+    //Create a text node
+    let link = document.createTextNode("Clink here to be directed to the iteration page");
+    //Append the text node to link element
+    a.appendChild(link);
+    //Title of link
+    a.title = "Iteration link";
+    //Link to iteration page
+    a.href = "https://kaidavey.github.io/grp109/team_project/iteration.html";
+    //Append the a element to the body (the div after form element)
+    document.getElementById("linktoI").appendChild(a);
+  }
