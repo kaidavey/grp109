69c785d1c8a91615551310de81a48b3040680ddf
Kai <k.davey4@icloud.com>
2025-02-20 19:13:18 -0500

    js changes to login page

---
+++ team_project/js/login.js
@@ -0,0 +1,32 @@
+function getUserInputs() {
+    // Store username input
+    var username = document.getElementById("username").value;
+    // Store useremail input
+    var useremail = document.getElementById("useremail").value;
+    // Store error message
+    var errorMsg = document.getElementById("error");
+    // Store next page button
+    var nextPageButton = document.getElementById("nextPage");
+
+    // Reset all messages on page
+    document.getElementById("headerMsg").innerHTML = "We're Glad You're Here!";
+    document.getElementById("welcomeMsg").innerHTML = "Please log in to access the Bellevue College Accepted Student Portal. Enter your full name and email below.";
+    errorMsg.textContent = "";
+
+    // Validation for text input
+    var validInput = username.length > 0 && useremail.length > 0;
+
+    // Passed validation?
+    if (validInput) {
+        console.log(username, useremail); //console.log for debugging
+        //Change H1 message to include username
+        document.getElementById("headerMsg").innerHTML = "We're Glad You're Here, " + username + "!";
+        //Change welcome message and include username + useremail
+        document.getElementById("welcomeMsg").innerHTML = "Congratulations and welcome to Bellevue College, " + username + "! Your email is " + useremail + ", so remember it because it's important!";
+        nextPageButton.style.display = "block";
+    } else {
+        nextPageButton.style.display = "none";
+        console.log("User's name or email was invalid.");
+        errorMsg.textContent = "Please enter a valid name and email.";
+    }
+}
