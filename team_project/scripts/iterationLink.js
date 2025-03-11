d7fa6cb3d6553919164662ee3519c9bfc371f77f
Kai <k.davey4@icloud.com>
2025-03-11 09:49:42 -0700

    view flow changed

--- team_project/scripts/iterationLink.js
+++ team_project/scripts/iterationLink.js
@@ -6,13 +6,13 @@ function newlink(){
     //Create a element
     let a = document.createElement('a');
     //Create a text node
-    let link = document.createTextNode("Click here to be directed to the iteration page.");
+    let link = document.createTextNode("Click here to be directed to the Home Page.");
     //Append the text node to link element
     a.appendChild(link);
     //Title of link
-    a.title = "Iteration link";
+    a.title = "Home link";
     //Link to iteration page
-    a.href = "iteration.html";
+    a.href = "home.html";
     //Append the a element to the body (the div after form element)
-    document.getElementById("linktoI").appendChild(a);
+    document.getElementById("linktoH").appendChild(a);
   }
